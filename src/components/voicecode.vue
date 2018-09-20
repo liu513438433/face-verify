ca6a0440e8c874d5f5c3307ad34d8c380f8ec53a
v_liusitong <v_liusitong@baidu.com>
2018-09-20 11:05:58 +0800

    update

---
+++ src/components/voicecode.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div v-for="(item,index) in voiceCode" :key="index" class="groundF">{{item}}</div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                voiceCode: []
+            }
+        }
+    }
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+    .container 
+        display flex
+        justify-content center
+        background-color black
+        .groundF
+            margin 0 auto 
+            font-size 60px
+            -webkit-background-clip text
+            -webkit-text-fill-color rgba(255,255,255,.1)
+            background-image url("./assets/image/test.png")
+            background-position 58% 10%
+            :nth-child(1)
+                transition: background-position 1.5s linear 0s;
+            :nth-child(2)
+                transition: background-position 1.5s linear 1.5s;
+            :nth-child(3)
+                transition: background-position 1.5s linear 3s;
+            :nth-child(4)
+                transition: background-position 1.5s linear 4.5s;
+</style>
